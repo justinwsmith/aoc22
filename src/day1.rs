fb286834756aba6c2b08d68ec70d77cfdd52a48f
Justin Smith <justsmth@amazon.com>
2022-12-01 10:29:48 -0500

    Day1 - part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::BinaryHeap;
 use std::str::FromStr;
 
 pub fn solve() -> u32 {
@@ -19,4 +20,28 @@ pub fn solve() -> u32 {
     }
 
     max_elf
+}
+
+pub fn solve2() -> u32 {
+    let input = include_str!("../inputs/day1.txt");
+
+    let mut heap : BinaryHeap<u32> = BinaryHeap::new();
+    let mut current_elf = 0u32;
+
+    for line in input.lines() {
+        let line = line.trim();
+        if line.len() == 0 {
+            heap.push(current_elf);
+            current_elf = 0;
+        } else {
+            current_elf += u32::from_str(line).unwrap();
+        }
+    }
+
+    let mut top_elfs_load = 0;
+    for _ in 1..=3 {
+        top_elfs_load += heap.pop().unwrap()
+    }
+
+    top_elfs_load
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod day1;
 
 fn main() {
     println!("Day1: {}", day1::solve());
+    println!("Day1-2: {}", day1::solve2());
 }
